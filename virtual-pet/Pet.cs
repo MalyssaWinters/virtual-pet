25523899da24908c20d466c172034b90afb97789
Malyssa Winters <malyssawinters5@gmail.com>
2016-06-24 13:22:09 -0400

    initial set up

---
+++ virtual-pet/Pet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace virtual_pet
+{
+    class Pet
+    {
+        public string PetName { get; set; }
+        public int Feed { get; set; }
+        public int Waste { get; set; }
+        public int Exercise { get; set; }
+        public int VisitVet { get; set; }
+        
+        public void MainMenu()
+        {
+            Console.WriteLine("\nChoose an option below to interact with " + PetName +".");
+            Console.WriteLine("\n1. Feed");
+            Console.WriteLine("2. Waste");
+            Console.WriteLine("3. Exercise");
+            Console.WriteLine("4. Visit Vet");
+        }
+
+        
+    }
+}
--- virtual-pet/Program.cs
+++ virtual-pet/Program.cs
@@ -8,8 +8,30 @@ namespace virtual_pet
 {
     class Program
     {
+        /*
+        * Welcome message
+        * Create pet name
+        * 
+        * Pull lever
+        * Randomly rolls around and gives you 
+        * Machine tells you if you won or not
+        */
+    
         static void Main(string[] args)
         {
+            Pet myPet = new Pet();
+
+            Console.WriteLine("Welcome, meet your new pet cat!");
+            Console.WriteLine("\nPlease name your cat.");
+            myPet.PetName = Console.ReadLine();
+                        
+            while (true)
+            {
+                myPet.MainMenu();
+                Console.ReadKey();
+            }
+                                  
         }
+        
     }
 }
